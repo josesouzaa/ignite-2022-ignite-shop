554fa6bcd88e1cd703f7219f52182a74f2f1d375
josesouzaa <josee.souzaaa@gmail.com>
2022-10-19 21:57:58 -0300

    Integração com o Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
+
+import Stripe from 'stripe'
+import { stripe } from '../lib/stripe'
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
 import { styled } from '../styles'
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 
 const Container = styled('main', {
   display: 'flex',
@@ -48,7 +49,8 @@ const Product = styled('a', {
     transition: 'all 0.2s ease-in-out',
 
     strong: {
-      fontSize: '$lg'
+      fontSize: '$lg',
+      color: '$gray100'
     },
 
     span: {
@@ -66,7 +68,16 @@ const Product = styled('a', {
   }
 })
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -76,45 +87,48 @@ export default function Home() {
 
   return (
     <Container ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camiseta1} alt="Camiseta 1" width={520} height={480} />
+      {products.map((product) => {
+        return (
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide" key={product.id}>
+              <Image src={product.imageUrl} alt="" width={520} height={480} />
 
-        <footer>
-          <strong>Camiseta 1</strong>
+              <footer>
+                <strong>{product.name}</strong>
 
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta2} alt="Camiseta 2" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta 2</strong>
-
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} alt="Camiseta 3" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta 3</strong>
-
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} alt="Camiseta 3" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta 3</strong>
-
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        )
+      })}
     </Container>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2
+  }
 }
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,127 @@
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+
+import { styled } from '../../styles'
+
+const Container = styled('main', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  alignItems: 'stretch',
+  gap: '4rem',
+  maxWidth: 1180,
+  margin: '0 auto'
+})
+
+const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 656,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover'
+  }
+})
+
+const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300'
+  },
+
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300'
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300'
+  },
+
+  button: {
+    marginTop: 'auto',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+
+    '&:hover': {
+      backgroundColor: '$green300'
+    }
+  }
+})
+
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  return (
+    <Container>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </Container>
+  )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1
+  }
+}
